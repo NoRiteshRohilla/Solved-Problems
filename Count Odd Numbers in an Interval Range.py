98399dcfbcd045fdf5924f730343c5d569741002
Ritesh Rohilla <65576858+riteshrohilla@users.noreply.github.com>
2022-07-12 19:08:43 +0530

    Create Count Odd Numbers in an Interval Range.py

---
+++ Count Odd Numbers in an Interval Range.py
@@ -0,0 +1,10 @@
+# class Solution:
+#     def countOdds(self, low: int, high: int) -> int:
+        
+
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        count = math.ceil((high-low)/2)
+        if (high % 2 != 0 and low % 2 !=0):
+            count+=1
+        return count
