0608b418af9342a400d7e61a14f8fe44eb631029
Ritesh Rohilla <65576858+riteshrohilla@users.noreply.github.com>
2023-10-04 22:25:59 +0530

    Create helloWorldfn.js

---
+++ helloWorldfn.js
@@ -0,0 +1,23 @@
+/**
+ * @return {Function}
+ */
+
+// var createHelloWorld = function() {
+//     return function() {
+//         return "Hello World";
+//     }
+// };
+
+// var createHelloWorld = function() {
+//     return function(...args) {
+//         return "Hello World"
+//     }
+// };
+
+// var createHelloWorld = function() {
+//     return (...args) => "Hello World";
+// };
+
+var createHelloWorld = function() {
+    return () => "Hello World";
+};
